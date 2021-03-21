e0d4b8bb93eeaf4d847662961e4d27d8a2c9353f
ElitLien <supervlad586@gmail.com>
2021-03-21 22:41:55 +0200

    first master commit

---
+++ Lab_2.2/UnitTest 2.2/UnitTest 2.2.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_2.2/Stek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest22
+{
+	TEST_CLASS(UnitTest22)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
